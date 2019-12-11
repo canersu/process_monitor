a56870f060812c3e6b0603497e868254bd7b0b3a
canersu <canersu34@gmail.com>
2019-12-11 20:01:48 +0300

    added kernel information

--- src/system.cpp
+++ src/system.cpp
@@ -21,8 +21,19 @@ Processor& System::Cpu() { return cpu_; }
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
-
+std::string System::Kernel() { 
+    //return string(); }
+    string os, version, kernel;
+    string line;
+    std::ifstream stream(LinuxParser::kProcDirectory + LinuxParser::kVersionFilename);
+    if(stream.is_open())
+    {
+        std::getline(stream, line);
+        std::istringstream linestream(line);
+        linestream >> os >> version >> kernel;
+    }
+    return kernel;
+}
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
