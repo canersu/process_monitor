9b2263d5102005227b5b65d96c871e0f3a6d9840
canersu <canersu34@gmail.com>
2019-12-18 14:52:36 +0300

    add clock format

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,9 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hr, min;
+    min = seconds/60;
+    hr = min/60;
+    return std::to_string(hr) + ":" + std::to_string(min%60) + ":" + std::to_string(seconds%60);
+}
